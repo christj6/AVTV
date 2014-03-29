0d86307f7295402c6df31772c4d9bc71b97e36eb
Jack Christiansen <christj6@tcnj.edu>
2014-03-28 21:35:07 -0400

    Update movies.rb

--- movies.rb
+++ movies.rb
@@ -9,6 +9,7 @@ require 'mechanize'
 
 agent = Mechanize.new
 
+#puts "Enter the name of a film director"
 #_searchTerm = gets.chomp
 _searchTerm = "Paul Thomas Anderson"
 
@@ -22,14 +23,26 @@ agent.get('http://www.imdb.com/') do |page|
   directorPage = search_result.link_with(text: result.inner_html).click
 
   page = Nokogiri::HTML(open(directorPage.uri.to_s))
+  #puts directorPage.uri.to_s
+  movieLinks = page.css('b a')
 
-  puts directorPage.uri.to_s
+  for i in 0..movieLinks.length-1
+  	#
+  	if movieLinks[i].to_s.include? "dr_" # retrieve the movies he/she directed
+  		#puts movieLinks[i]
+  		#moviePage = directorPage.link_with(text: movieLinks[i].inner_html).click
 
-  movies = Array.new
+  		puts directorPage.link_with(text: movieLinks[i].inner_html)
 
-  puts page.css('div div div div div div div div div b a')
+  		#page = Nokogiri::HTML(open(moviePage.uri.to_s))
+  		#runTime = page.css('time').to_s
+  		#puts runTime
+  	end
+  end
+
+
   
 
 
 
-  end
+ end

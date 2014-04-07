17b4005e4a0519bad868122ea75531fb5443fe31
caneroj1 <caneroj1@tcnj.edu>
2014-04-07 17:17:10 -0400

    Using the HAML gem to create HTML that can have ruby embedded.

---
+++ chart.rb
@@ -0,0 +1,40 @@
+require 'haml'
+
+def writeContents(arr, arr2, arr3, name)
+  ratingsHash = Hash.new()
+  seasonCount = 0
+  episodeCount = 1
+  arr.reverse.each do |v|
+    key = "S#{arr3[arr2.length - 1 - seasonCount].inner_html}E#{episodeCount}"
+    value = v
+    ratingsHash.store(key, value)
+    episodeCount += 1
+    if (episodeCount > arr2[seasonCount]) and (seasonCount < arr2.length) then
+      episodeCount = 1
+      seasonCount += 1
+    end
+  end
+  writeFile(ratingsHash, name)
+end
+
+def writeFile(hash, name)
+  ## open the HAML template
+  template = File.read('template.haml')
+  
+  ## use the HAML engine to add a method to the ratings hash that will render the HAML as HTML and 
+  # insert itself into the HTML so we can use its data
+  Haml::Engine.new(template).def_method(hash, :render, :title)
+  
+  ## open the text file to store the results and open the file that will be the output HTML
+  file = File.open("#{name}.txt", "w")
+  htmlFile = File.open("#{name}.html", "w")
+  
+  ## write the contents of the hash to the text file
+  hash.each do |key, value|
+    file.write("#{key}-#{value}\n")
+  end
+  
+  ## write the rendered HTML to an html file
+  htmlFile.write(hash.render(:title => name))
+end
+

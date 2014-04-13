740c06d7c831b6a244843e38cee5e47e3b917490
Jack Christiansen <christj6@tcnj.edu>
2014-04-13 13:58:59 -0400

    trying to merge

--- av_club_graph.rb
+++ av_club_graph.rb
@@ -1,3 +1,4 @@
+
 =begin
 
 Given the name of a television show, this program gathers the grades AV Club gave to every episode it reviewed,
@@ -51,7 +52,17 @@ require_relative 'chart.rb'
 
 agent = Mechanize.new
 
+## check command line arguments
+#  accepts only a single argument: either "jarow" or "reg"
+#  this tells the program which string matching technique to use when selecting the show to chart
 
+if ARGV.count != 1 then 
+  puts "Command-line arguments invalid. 1 required."
+  exit
+else 
+  command = ARGV[0]
+end
+ARGV.clear
 
 # AV Club Grades: A, A-, B+, B, B-, C+, C, C-, D+, D, D-, F
 # on metacritic, they correspond with: 100, 91, 83, 80, 75, 70, 67, 60, 58, 50, 42, 40 (SKIP), 33, 25, 16, 0
@@ -77,54 +88,64 @@ agent.get('http://www.avclub.com/tv/') do |page|
   puts "" # make it easier on the eyes
 
   if results.length > 1
-<<<<<<< HEAD
-  	# choice 1: allow the user to input their choice. Might get complicated later when
-  	# we have to create some whole other page for storing the possible options. If we're going with rails.
-  	# puts "Which one is it? Enter the corresponding number."
-  	# userChoice = gets.chomp
-  	# reviews = search_result.link_with(text: results[userChoice.to_i-1].inner_html).click
-  	# _show = results[userChoice.to_i-1].inner_html
-
-  	# choice 2: compare the string and make a guess on which one is most likely the one the user intended.
-  	jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-  	bestDistance = 0
-  	current = 0
-  	matchIndex = 0
-
-  	for i in 0..results.length-1
-  		choice = results[i].inner_html
-
-  		current = jarow.getDistance(_show.downcase,choice.downcase)
-  		print choice + ": \t" + (current*100).to_s + "% match"
-  		puts ""
-
-  		if current > bestDistance
-  			bestDistance = current
-  			matchIndex = i
-  		else
-  			# do nothing
-  		end
-  	end
-
-  	reviews = search_result.link_with(text: results[matchIndex.to_i].inner_html).click
-  	_show = results[matchIndex.to_i].inner_html
-
-=======
-  	puts "Which one is it? Enter the corresponding number."
-  	userChoice = gets.chomp
+    #puts "Which one is it? Enter the corresponding number."
+  	#userChoice = gets.chomp
   	#userChoice = userChoice.gsub! '&amp;','&'
-  	reviews = search_result.link_with(text: results[userChoice.to_i-1].inner_html).click
->>>>>>> parent of 7e005d3... messed with _show var
-  else
+  	#reviews = search_result.link_with(text: results[userChoice.to_i-1].inner_html).click
+    
+    ## create _showProper which is just the search term with the first letter of each word capitalized to match av club's style.
+    #  a found boolean is used to determine when the search is successful. this is needed because the (experts) part of the reg exp is optional, so it will match
+    #  any search term that has (experts) or (newbies) or nothing. i am counting on the user always desiring the expert rating (if it exists) and for that rating to
+    #  always come first.
+    
+    ## i also noticed that, for example, if i searched for "family gu", the search function would indeed return Family Guy as a valid result, which i assume is because 
+    #  of the fuzzy search, but the regexp below selected "Family Guy" correctly. This is because the regular expression matched as many characters as it could from 
+    #  the broken search term and got the right answer.
+    
+    _showProper = _show.split.map(&:capitalize).join(' ')
+    if command.eql?("jarow") then
+      puts "jarow match"
+      # choice 2: compare the string and make a guess on which one is most likely the one the user intended.
+      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
+      bestDistance = 0
+      current = 0
+      matchIndex = 0
+
+      for i in 0..results.length-1
+        choice = results[i].inner_html
+
+        current = jarow.getDistance(_show.downcase,choice.downcase)
+        print choice + ": \t" + (current*100).to_s + "% match"
+        puts ""
+
+        if current > bestDistance
+          bestDistance = current
+          matchIndex = i
+        else
+          # do nothing
+        end
+      end
+
+      reviews = search_result.link_with(text: results[matchIndex.to_i].inner_html).click
+      #_show = results[matchIndex.to_i].inner_html
+    elsif command.eql?("reg") 
+      puts "regular expression match"
+      reviews = nil
+      found = false
+      results.each do |i|
+        if !i.inner_html[/#{_showProper}(\s\(experts\))?/].nil? and !found then
+          reviews = search_result.link_with(text: i.inner_html).click
+          found = true
+        end
+      end
+    end
+  elsif results.length == 1
   	reviews = search_result.link_with(text: results[0].inner_html).click
+  else ## added additional error checking in case there are no search results
+    puts "No results matched your search term."
+    exit
   end
 
-<<<<<<< HEAD
-  _show = _show.downcase.tr(" ", "_")
-  #puts _show # --now "_show" is equal to the actual name of the show, not just whatever the user typed in.
-
-=======
->>>>>>> parent of 7e005d3... messed with _show var
   puts "" # make it easier on the eyes
 
 
@@ -206,18 +227,18 @@ agent.get('http://www.avclub.com/tv/') do |page|
   	end
   end
   #calls a function that outputs an html file for displaying the chart of the show
-  writeContents(graphY, episodesPerSeason, validSeasons, _show)
+  writeContents(graphY, episodesPerSeason, validSeasons, _showProper)
   	# writes a text file containing the numbers corresponding to each grade
-  	begin
-  		file = File.open("numberList.txt", "w")
-  		for i in 0..graphY.length-1
-  			file.write(graphY[graphY.length-1 - i].to_s + "\n")
-  		end 
-  	rescue IOError => e
-	  #some error occur, dir not writable etc.
-	ensure
-		file.close unless file == nil
-	end
+  	# begin
+#       file = File.open("numberList.txt", "w")
+#       for i in 0..graphY.length-1
+#         file.write(graphY[graphY.length-1 - i].to_s + "\n")
+#       end 
+#     rescue IOError => e
+#     #some error occur, dir not writable etc.
+#   ensure
+#     file.close unless file == nil
+#   end
 
 	puts ""
   	puts "" # make it easier on the eyes
